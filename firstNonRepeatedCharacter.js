eaae366d83c2a1ebe8d38a0160c76c1ab14164a0
Jordan Bishop <bishopj1292@gmail.com>
2017-08-10 19:29:11 -0500

    Implement refactored solution

--- firstNonRepeatedCharacter.js
+++ firstNonRepeatedCharacter.js
@@ -4,8 +4,24 @@
 Given an arbitrary input string, return the first non-repeating
 character. For strings with all repeats, return 'sorry'. */
 
-// Initial solutions:
+// Initial solution:
+function firstNonRepeatedCharacter(str) {
+  var strArr = str.split('');
+  var results = {};
+  for (var char of strArr) {
+    !results[char] ? results[char] = 1 : results[char]++;
+  }
+  for (var i of strArr) {
+    if (results[i] === 1) {
+      return i;
+    }
+  }
+  return 'sorry';
+}
 
+
+
+// My refactored solution:
 function firstNonRepeatedCharacter (string) {
   var strArr = string.split('');
   for (let char of strArr) {
